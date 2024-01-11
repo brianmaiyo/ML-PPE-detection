ba957d994f606fdf38393d2e050f395695f034b0
brianmaiyo <67601292+brianmaiyo@users.noreply.github.com>
2024-01-11 22:38:42 +0300

    initial

--- utils.py
+++ utils.py
@@ -11,29 +11,34 @@ def get_subdirs(b='.'):
             result.append(bd)
     return result
 
-
 def get_detection_folder():
     '''
         Returns the latest folder in a runs\detect
     '''
-    return max(get_subdirs(os.path.join('runs', 'detect')), key=os.path.getmtime)
+    detect_path = os.path.join('runs', 'detect')
+    detect_path_absolute = os.path.abspath(detect_path)
+
+    # Get the absolute paths of all subdirectories
+    subdirs_absolute = [os.path.abspath(subdir) for subdir in get_subdirs(detect_path)]
+
+    return max(subdirs_absolute, key=os.path.getmtime)
 
 def check_folders():
     paths = {
         'data_path' : 'data',
         'images_path' : 'data/images',
         'videos_path' : 'data/videos'
-        
     }
+    
     # Check whether the specified path exists or not
     notExist = list(({file_type: path for (file_type, path) in paths.items() if not os.path.exists(path)}).values())
     
     if notExist:
-        print(f'Folder {notExist} does not exist. We will created')
+        print(f'Folder {notExist} does not exist. We will create it.')
         # Create a new directory because it does not exist
         for folder in notExist:
             os.makedirs(folder)
             print(f"The new directory {folder} is created!")
-  
-        
-        
+
+# Uncomment the next line to check folders when this script is executed
+# check_folders()
